03563dcc33660491f2f5e57f14f92b11f9eca356
Jenna <jennashamza@gmail.com>
2022-11-04 10:06:12 -0700

    alert styling

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { FiAlertTriangle } from "react-icons/fi";
 
 const Modal = ({ shifts, nurses, toggleModal }) => {
   const [selectedShift, setSelectedShift] = useState("");
@@ -27,11 +28,26 @@ const Modal = ({ shifts, nurses, toggleModal }) => {
     );
   };
 
-  // const nurseSchedVerified = async () => {
+  // const nurseSchedVerified = () => {
   //   const nurseData = nurses.filter((nurse) => nurse.id === selectedNurse)[0];
+  //   // already assigned shifts
   //   const assignedShifts = shifts.filter(
   //     (shift) => shift.nurse_id === nurseData.id
   //   );
+  //   // desired shift
+  //   const shiftData = shifts.filter((shift) => shift.id == selectedShift);
+  //   const shiftStart = shiftData[0].start;
+  //   const shiftEnd = shiftData[0].end;
+  //   // check if desired shift start time or end time lies between any of assigned shifts
+  //   for (let i = 0; i < assignedShifts.length; i++) {
+  //     const shift = assignedShifts[i];
+  //     if (
+  //       (shift.start >= shiftStart && shift.start <= shiftEnd) ||
+  //       (shift.end >= shiftStart && shift.end <= shiftEnd)
+  //     )
+  //       return false;
+  //   }
+  //   return true;
   // };
 
   const assignShift = async () => {
@@ -102,10 +118,15 @@ const Modal = ({ shifts, nurses, toggleModal }) => {
               ))}
             </select>
           </label>
-          <button>SAVE ASSIGNMENT</button>
           {qualErrorMsg ? (
-            <div>This nurse isn't qualified to work the chosen shift</div>
+            <div className="error-msg">
+              <span>
+                <FiAlertTriangle color="orange" fontSize="1.5em" />
+              </span>
+              This nurse isn't qualified to work the chosen shift
+            </div>
           ) : null}
+          <button>SAVE ASSIGNMENT</button>
         </form>
       </div>
     </div>
